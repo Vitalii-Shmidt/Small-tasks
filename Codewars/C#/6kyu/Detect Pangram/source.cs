8daff36b3643451ee1b208152fe8676f0ac70c35
theRevival <vitalii.revival@gmail.com>
2020-05-26 21:46:48 +0300

    JS, Coursera

--- Codewars/C#/6kyu/Detect Pangram/source.cs
+++ Codewars/C#/6kyu/Detect Pangram/source.cs
@@ -1,13 +1,15 @@
 // ref: https://www.codewars.com/kata/545cedaa9943f7fe7b000048
-
 using System;
 using System.Linq;
+
 public static class Kata {
- public static bool IsPangram(string str) {
-            var uniqueChars = str.Select(x => x)
-                                 .Distinct()
-                                 .ToList();
-            uniqueChars.RemoveRange(uniqueChars.Count - 1, 1);
-            return uniqueChars.Count < 26 ? false : true; // !uniqueChars.Count < 26 || uniqueChars.Count > 26. idk
+      public static bool IsPangram(string str) {
+        return str.ToLower()
+              .Select(ch => ch)
+              .Where((ch) => 
+                    ((Convert.ToInt32(ch) >= 97) && (Convert.ToInt32(ch) <= 122)))
+              .Distinct()
+              .ToList()
+              .Count == 26;
         }
 }
