15cd72e2d19e94468ac7dbed94c2777c5007b226
theRevival <vitalii.revival@gmail.com>
2020-07-10 01:04:25 +0300

    kek

--- Codewars/C#/5kyu/PaginationHelper/source.cs
+++ Codewars/C#/5kyu/PaginationHelper/source.cs
@@ -1,79 +1,77 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
-namespace Codewars {
-    public class PagnationHelperT {
-         summary
-         List of our in-comming items
-         summary
-        private ListT _items;
+    public class PagnationHelper<T> {
+        /// <summary>
+        /// List of our in-comming items
+        /// </summary>
+        private List<T> _items;
 
-         summary
-         Variable, stored an item count that may be placed on one page
-         summary
+        /// <summary>
+        /// Variable, stored an item count that may be placed on one page
+        /// </summary>
         private int _itemsPerPage;
 
-         summary
-         Constructor, takes in a list of items and the number of items that fit within a single page
-         summary
-         param name=collectionA list of itemsparam
-         param name=itemsPerPageThe number of items that fit within a single pageparam
-        public PagnationHelper(IListT collection, int itemsPerPage) {
-            _items = new ListT(collection);
+        /// <summary>
+        /// Constructor, takes in a list of items and the number of items that fit within a single page
+        /// </summary>
+        /// <param name="collection">A list of items</param>
+        /// <param name="itemsPerPage">The number of items that fit within a single page</param>
+        public PagnationHelper(IList<T> collection, int itemsPerPage) {
+            _items = new List<T>(collection);
             _itemsPerPage = itemsPerPage;
         }
 
-         summary
-         The number of items within the collection
-         summary
+        /// <summary>
+        /// The number of items within the collection
+        /// </summary>
         public int ItemCount {
             get {
                 return _items.Count;
             }
         }
 
-         summary
-         The number of pages
-         summary
+        /// <summary>
+        /// The number of pages
+        /// </summary>
         public int PageCount {
             get {
-                if(ItemCount == 0  _itemsPerPage = 0) {
+                if(ItemCount == 0 || _itemsPerPage <= 0) {
                     return -1;
                 }
-                var count = ItemCount  _itemsPerPage;
-                return ItemCount % _itemsPerPage == 0  count  ++count;
+                var count = ItemCount / _itemsPerPage;
+                return ItemCount % _itemsPerPage == 0 ? count : ++count;
             }
         }
 
-         summary
-         Returns the number of items in the page at the given page index 
-         summary
-         param name=pageIndexThe zero-based page index to get the number of items forparam
-         returnsThe number of items on the specified page or -1 for pageIndex values that are out of rangereturns
+        /// <summary>
+        /// Returns the number of items in the page at the given page index 
+        /// </summary>
+        /// <param name="pageIndex">The zero-based page index to get the number of items for</param>
+        /// <returns>The number of items on the specified page or -1 for pageIndex values that are out of range</returns>
         public int PageItemCount(int pageIndex) {
-            if(pageIndex = PageCount  pageIndex  0) {
+            if(pageIndex >= PageCount || pageIndex < 0) {
                 return -1;
             }
-            if(pageIndex == 0) {
-                return _itemsPerPage;
+            if(pageIndex == PageCount - 1) {
+                return ItemCount % _itemsPerPage;
             }
-            return Math.Abs(ItemCount - _itemsPerPage  pageIndex);
+            
+            return _itemsPerPage;
         }
 
-         summary
-         Returns the page index of the page containing the item at the given item index.
-         summary
-         param name=itemIndexThe zero-based index of the item to get the pageIndex forparam
-         returnsThe zero-based page index of the page containing the item at the given item index or -1 if the item index is out of rangereturns
+        /// <summary>
+        /// Returns the page index of the page containing the item at the given item index.
+        /// </summary>
+        /// <param name="itemIndex">The zero-based index of the item to get the pageIndex for</param>
+        /// <returns>The zero-based page index of the page containing the item at the given item index or -1 if the item index is out of range</returns>
         public int PageIndex(int itemIndex) {
-            if(itemIndex  0  itemIndex = ItemCount) {
+            if(itemIndex < 0 || itemIndex >= ItemCount) {
                 return -1;
             }
             var page = 0;
             var counter = 1;
 
-            for(int i = 0; i  itemIndex; ++i, ++counter) {
+            for(int i = 0; i < itemIndex; ++i, ++counter) {
                 if(counter == _itemsPerPage) {
                     ++page;
                     counter = 0;
@@ -83,4 +81,3 @@ namespace Codewars {
             return page;
         }
     }
-}
