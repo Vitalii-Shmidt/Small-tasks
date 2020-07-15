b8215a23206c287d4e8b6917686e8c34ee0435bb
theRevival <vitalii.revival@gmail.com>
2020-07-16 00:35:26 +0300

    Really good description, with the help of which you can easily get, what happened in the last commit, you know.

---
+++ Codewars/C#/4kyu/Most frequently used words in a text/source.cs
@@ -0,0 +1,30 @@
+// ref: https://www.codewars.com/kata/51e056fe544cf36c410000fb
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+public class TopWords
+{
+           public static List<string> Top3(string s) {
+            var mostFrequentlyUsedWords = s.Trim()
+                                           .Split(' ', ',', ':', '/')
+                                           .Select(x => x.ToLower())
+                                           .GroupBy(val => val)
+                                           .OrderByDescending(gr => gr.Count())
+                                           .Select(x => x.Key);
+           
+            var ans = new List<string>();
+
+            foreach(var word in mostFrequentlyUsedWords) {
+               if(ans.Count() == 3) {
+                    break;
+                } 
+              if(!string.IsNullOrWhiteSpace(word)) {
+                    ans.Add(word);
+                }
+               
+            }
+
+            return ans;
+        }
+}
