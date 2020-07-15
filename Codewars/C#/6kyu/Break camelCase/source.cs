7d5a7fc2304e757c4d279ee777bd3d60c54f4e09
theRevival <vitalii.revival@gmail.com>
2020-07-15 22:47:11 +0300

    Really good description, with the help of which you can easily get, what happened in the last commit, you know.

--- Codewars/C#/6kyu/Break camelCase/source.cs
+++ Codewars/C#/6kyu/Break camelCase/source.cs
@@ -1,21 +1,28 @@
 // ref: https://www.codewars.com/kata/5208f99aee097e6552000148
 
 public class Kata {
-        public static string BreakCamelCase(string str) {
-            var ans = "";
-            int i = 0;
-            foreach(var ch in str) {
-                ans += ch;
-                if (i == str.Length - 1) {
-                    break;
-                }
-                if (char.IsUpper(str[i + 1])) {
-                    ans += " ";
-                }
-  
-                ++i;
+    
+    // New solution    
+    public static string BreakCamelCase(string str) {
+        return string.Join("", str.Select(x => char.IsUpper(x) ? " " + x.ToString() : x.ToString()));
+    }     
+    
+    // An older solution
+    public static string BreakCamelCase(string str) {
+        var ans = "";
+        int i = 0;
+        foreach(var ch in str) {
+            ans += ch;
+            if (i == str.Length - 1) {
+                break;
             }
-
-            return ans;
+            if (char.IsUpper(str[i + 1])) {
+                ans += " ";
+            }
+  
+            ++i;
         }
+        
+        return ans;
+    }
 }
